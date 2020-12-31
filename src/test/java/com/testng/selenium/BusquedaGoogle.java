9d2fa245fd3b433332ffe5fcc2b4563dc0c7df0d
Francisco Mora <francisco.m.collao@gmail.com>
2020-12-31 17:00:29 -0300

    Se agrega ejemplo basico.

---
+++ src/test/java/com/testng/selenium/BusquedaGoogle.java
@@ -0,0 +1,23 @@
+package com.testng.selenium;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class BusquedaGoogle {
+	
+	@BeforeClass
+	public void setup() {
+		
+	}
+	
+	@Test
+	public void buscarEnGoogle() {
+		
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		
+	}
+}
