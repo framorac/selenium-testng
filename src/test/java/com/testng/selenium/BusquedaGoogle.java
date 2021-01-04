300116ee24d9ddc4fc438d8a6fd94dcca0cbe2e5
Francisco Mora <francisco.m.collao@gmail.com>
2021-01-04 16:26:25 -0300

    Se completa ejemplo BusquedaGoogle

--- src/test/java/com/testng/selenium/BusquedaGoogle.java
+++ src/test/java/com/testng/selenium/BusquedaGoogle.java
@@ -1,17 +1,25 @@
 package com.testng.selenium;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 
 public class BusquedaGoogle {
 	private WebDriver driver;
 	
 	@BeforeClass
 	public void setup() {
-		System.setProperty("webdriver.gecko.driver", "./src/test/resources/driver/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "./src/test/resources/drivers/geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
@@ -19,7 +27,11 @@ public class BusquedaGoogle {
 	
 	@Test
 	public void buscarEnGoogle() {
-		
+		WebElement cajaBusqueda = driver.findElement(By.name("q"));
+		cajaBusqueda.clear();
+		cajaBusqueda.sendKeys("selenium", Keys.ENTER);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		assertEquals("selenium - Buscar con Google", driver.getTitle());
 	}
 	
 	@AfterClass
