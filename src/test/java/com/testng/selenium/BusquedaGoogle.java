3594557a47ecffd5de7729be7cf66bf10d07cff7
Francisco Mora <francisco.m.collao@gmail.com>
2021-01-04 13:52:46 -0300

    Se actualizan drivers.

--- src/test/java/com/testng/selenium/BusquedaGoogle.java
+++ src/test/java/com/testng/selenium/BusquedaGoogle.java
@@ -1,14 +1,18 @@
 package com.testng.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class BusquedaGoogle {
+	WebDriver driver;
 	
 	@BeforeClass
 	public void setup() {
-		
+		System.setProperty("webdriver.gecko.driver", "./src/test/resources/driver/")
+		driver = new FirefoxDriver()
 	}
 	
 	@Test
