da0b20351aade36be3c80a222def293a15b088bb
Francisco Mora <francisco.m.collao@gmail.com>
2021-01-07 14:34:04 -0300

    Subida inicial clase CrossBrowserTesting.java

---
+++ src/test/java/com/testng/selenium/CrossBrowserTesting.java
@@ -0,0 +1,25 @@
+package com.testng.selenium;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CrossBrowserTesting {
+	
+	@BeforeClass
+	public void setup() {
+		
+	}
+	
+	@Test
+	public void busquedaMultiNavegador() {
+		
+	}
+	
+	
+	@AfterClass
+	public void tearDown() {
+		
+	}
+
+}
