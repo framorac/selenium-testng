2c7a3c97af67a5e8591b053881c69571c7644d8c
Francisco Mora <francisco.m.collao@gmail.com>
2021-01-21 17:41:45 -0300

    Se actualiza clase SeleniumSelectores.java

--- src/test/java/com/testng/selenium/SeleniumSelectores.java
+++ src/test/java/com/testng/selenium/SeleniumSelectores.java
@@ -1,6 +1,7 @@
 package com.testng.selenium;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 /**
@@ -13,11 +14,13 @@ public class SeleniumSelectores {
 	
 	@BeforeClass
 	public void setup() {
-		
+		System.setProperty("webdriver.gecko.driver", "./src/test/resources/drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
 	}
 	
 	@AfterClass
 	public void tearDown() {
-		
+		driver.close();
+		driver.quit();
 	}
 }
