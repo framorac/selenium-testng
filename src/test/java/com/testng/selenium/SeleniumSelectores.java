dde44dba4a402779f9d9164cd1db085a5614844a
Francisco Mora <francisco.m.collao@gmail.com>
2021-01-28 20:41:24 -0300

    Se crea test selectorById en clase SeleniumSelectores.java

--- src/test/java/com/testng/selenium/SeleniumSelectores.java
+++ src/test/java/com/testng/selenium/SeleniumSelectores.java
@@ -1,9 +1,12 @@
 package com.testng.selenium;
 
+import static org.testng.Assert.fail;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class SeleniumSelectores {
 	private WebDriver driver;
@@ -12,6 +15,11 @@ public class SeleniumSelectores {
 	public void setup() {
 		System.setProperty("webdriver.gecko.driver", "./src/test/resources/drivers/geckodriver.exe");
 		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void selectorById() {
+		fail("No implementado");
 	}
 	
 	@AfterClass
